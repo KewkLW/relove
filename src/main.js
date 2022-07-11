87c734e476766d81722bbbaf32622178ecca7eb9
KewkLW <57611539+KewkLW@users.noreply.github.com>
2022-07-10 23:50:53 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/YoEepC9m5Imi5Jn3/scene.splinecode');
